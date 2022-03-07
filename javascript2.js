5f2be57a17db2ab581c75ab7df618dff1f9de7b3
Lina Cardona <linamaria126@gmail.com>
2022-03-07 15:49:11 -0500

    first commit

---
+++ javascript2.js
@@ -0,0 +1,22 @@
+
+var cantidadDeVentanasEnMiCasa = 5
+var soyHumano = true
+var miGustoDePizzaPreferido = "Peperoni"
+
+
+console.log(cantidadDeVentanasEnMiCasa)
+console.log(soyHumano)
+console.log(miGustoDePizzaPreferido)
+
+
+var x = false
+console.log(x)
+
+var x = 'Hola'
+console.log(x)
+
+var x = 0
+console.log(x)
+
+var x = 'Minions'
+console.log(x)
